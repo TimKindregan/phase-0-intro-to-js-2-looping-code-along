4b0b7f9ae519804a2a0ef7b5c1a3842b2f1cfbdf
Tim <timothyj.kindregan@gmail.com>
2022-12-09 00:48:25 -0500

    commit tree ebd7e3f19dc503c562e4fefcb3f5d015edf4760f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYou = [];
+    for (let n = 0; n < names.length; n++) {
+        thankYou.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`)
+    }
+    return(thankYou);
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
